4213c545b7d5c7277d92c73cba34749ad3b84a05
VanSail <13410850124@163.com>
2026-08-03 21:24:39 +0800

    chore(ContribHeatmap): drop unused useMemo import

--- src/components/ContribHeatmap.tsx
+++ src/components/ContribHeatmap.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './ContribHeatmap.module.css';
 
